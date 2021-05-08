9e920e98deef7d4d2976121a65f73b4844793636
theethaj <bank30827@gmail.com>
2021-05-08 11:03:11 +0700

    Add cors

--- src/main/java/ku/product/controller/ProductController.java
+++ src/main/java/ku/product/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5000")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
